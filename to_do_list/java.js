2f611c8982669df1764e559b73efe5e52d403374
Catharina <catharina2002@gmail.com>
2024-10-22 13:59:54 +0200

    heart

--- to_do_list/java.js
+++ to_do_list/java.js
@@ -1,40 +1,32 @@
-"use strict";
-
 const ulTodo = document.querySelector("#todos");
 const inputTodo = document.querySelector("#todo");
 const button = document.querySelector("button");
 
-const model = ["Husk at ring til mormor"];
+const model = ["Husk at ringe til mormor"];
 init();
 
 function init() {
-    button.addEventListener("click", button_click);
-    upDateView();
-  }
-  
-  function addTodoToModel(todo) {
+  button.addEventListener("click", button_click);
+  upDateView();
+}
+
+function addTodoToModel(todo) {
+  if (todo.trim() !== "") {
     model.push(todo);
   }
-  function removeTodoFromModel(id) {
-    model.splice(id, 1);
-  }
-  
-  function upDateView() {
-    ulTodo.innerHTML = "";
-    model.forEach((each, i) => {
-      ulTodo.innerHTML += `<li class="clicktodo" data-id="${i}">${each}</li> `;
-    });
-  
-    document.querySelectorAll(".clicktodo").forEach((each) => {
-      each.addEventListener("click", klikspan);
-    });
-  }
-  
-  function button_click() {
-    addTodoToModel(inputTodo.value);
-    upDateView();
-  }
-  function klikspan(evt) {
-    removeTodoFromModel(evt.target.dataset.id);
-    upDateView();
-  }
+}
+
+function upDateView() {
+  ulTodo.innerHTML = "";
+  model.forEach((each, i) => {
+    const li = createTaskElement(each); // Brug den nye funktion.
+    li.dataset.id = i; // Tildel ID til hver task.
+    ulTodo.appendChild(li);
+  });
+}
+
+function button_click() {
+  addTodoToModel(inputTodo.value);
+  inputTodo.value = ""; // Ryd inputfeltet.
+  upDateView();
+}
