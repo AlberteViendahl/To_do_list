40f5934ed658b1eb1235389372258df07be46a3b
Catharina <catharina2002@gmail.com>
2024-10-23 15:38:28 +0200

    virk

--- to_do_list/java.js
+++ to_do_list/java.js
@@ -3,38 +3,94 @@
 const ulTodo = document.querySelector("#todos");
 const inputTodo = document.querySelector("#todo");
 const button = document.querySelector("button");
+const completedSection = document.getElementById("completed-section");
 
-const model = ["Husk at ringe til mormor"];
+const todoModel = ["Husk at ringe til mormor"]; // To-do tasks
+const completedModel = []; 
+
 init();
 
 function init() {
-    button.addEventListener("click", button_click);
-    upDateView();
+  button.addEventListener("click", button_click);
+  updateView();
+}
+
+function addTodoToModel(todo) {
+  if (todo) {
+    todoModel.push(todo);
   }
-  
-  function addTodoToModel(todo) {
-    model.push(todo);
-  }
-  function removeTodoFromModel(id) {
-    model.splice(id, 1);
-  }
-  
-  function upDateView() {
-    ulTodo.innerHTML = "";
-    model.forEach((each, i) => {
-      ulTodo.innerHTML += `<li class="clicktodo" data-id="${i}">${each}</li> `;
+}
+
+function removeTodoFromModel(id) {
+  todoModel.splice(id, 1);
+}
+
+function removeCompletedFromModel(id) {
+  completedModel.splice(id, 1);
+}
+
+function completeTask(id) {
+  const task = todoModel[id]; 
+  completedModel.push(task); 
+  removeTodoFromModel(id);    
+  updateView();               
+}
+
+function undoCompleteTask(id) {
+  const task = completedModel[id]; 
+  todoModel.push(task);            
+  removeCompletedFromModel(id);    
+  updateView();                  
+}
+
+function updateView() {
+  ulTodo.innerHTML = ""; 
+  todoModel.forEach((task, i) => {
+    ulTodo.innerHTML += `
+      <li class="clicktodo flex" data-id="${i}">
+        <span>${task}</span>
+        <img src="./svg/empty_heart.svg" alt="hjerte" class="heart" />
+      </li>
+    `;
+  });
+
+  completedSection.innerHTML = `<h1>Completed tasks</h1>`;
+  completedModel.forEach((task, i) => {
+    completedSection.innerHTML += `
+      <li class="flex" data-id="${i}"><span>${task}</span><img src="./svg/filled_heart.svg" alt="hjerte" class="heart" /></li>
+    `;
+  });
+
+  document.querySelectorAll("#todos .heart").forEach((heart, index) => {
+    heart.addEventListener("click", function () {
+      completeTask(index); 
     });
-  
-    document.querySelectorAll(".clicktodo").forEach((each) => {
-      each.addEventListener("click", klikspan);
+  });
+
+  document.querySelectorAll("#completed-section .heart").forEach((heart, index) => {
+    heart.addEventListener("click", function () {
+      undoCompleteTask(index); 
     });
+  });
+
+  document.querySelectorAll(".clicktodo span").forEach((each) => {
+    each.addEventListener("click", klikspan); 
+  });
+}
+
+function button_click() {
+  const newTodo = inputTodo.value.trim(); 
+  if (newTodo) {
+    addTodoToModel(newTodo);
+    inputTodo.value = "";
+    updateView();
+  } else {
+    alert("Please enter a task!");
   }
-  
-  function button_click() {
-    addTodoToModel(inputTodo.value);
-    upDateView();
-  }
-  function klikspan(evt) {
-    removeTodoFromModel(evt.target.dataset.id);
-    upDateView();
-  }
+}
+
+function klikspan(evt) {
+  const id = evt.target.closest("li").dataset.id;
+  removeTodoFromModel(id);
+  updateView();
+}
