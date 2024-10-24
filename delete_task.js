418ceaf3f5c00dcfaee121aa9cea78123ba456f1
Catharina <catharina2002@gmail.com>
2024-10-24 10:40:14 +0200

    bæredygtighed first

--- delete_task.js
+++
@@ -1,7 +0,0 @@
-const tasks = document.querySelectorAll(".delete_task");
-
-tasks.forEach((task) => {
-    task.addEventListener("click", function () {
-        task.parentElement.remove();
-    });
-});
